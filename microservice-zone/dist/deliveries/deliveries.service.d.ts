6bd2644ef1ee321b0c2b1cfbac163eda8b2c0e61
ciggy <alexandermassietti@gmail.com>
2025-04-29 21:54:03 -0300

    Creación de DTO Location, Delivery, logica implementada a la hora de CreateDelivery. Falta agregar lógica de delivery-status. Implementación de class-validator en DTO CreateDelivery y CreateLocation, falta implementar class-validator en CreateStatusDelivery.

--- microservice-zone/dist/deliveries/deliveries.service.d.ts
+++ microservice-zone/dist/deliveries/deliveries.service.d.ts
@@ -1,9 +1,17 @@
+import { Repository } from 'typeorm';
 import { CreateDeliveryDto } from './dto/create-delivery.dto';
 import { UpdateDeliveryDto } from './dto/update-delivery.dto';
+import { Delivery } from './entities/delivery.entity';
+import { Location } from 'src/location/entities/location.entity';
+import { DeliveryStatus } from 'src/delivery-status/entities/delivery-status.entity';
 export declare class DeliveriesService {
-    create(createDeliveryDto: CreateDeliveryDto): string;
-    findAll(): string;
-    findOne(id: number): string;
-    update(id: number, updateDeliveryDto: UpdateDeliveryDto): string;
-    remove(id: number): string;
+    private readonly deliveryRepository;
+    private readonly locationRepository;
+    private readonly deliveryStatusRepository;
+    constructor(deliveryRepository: Repository<Delivery>, locationRepository: Repository<Location>, deliveryStatusRepository: Repository<DeliveryStatus>);
+    create(createDeliveryDto: CreateDeliveryDto): Promise<Delivery>;
+    findAll(): Promise<Delivery[]>;
+    findOne(id: number): Promise<Delivery | null>;
+    update(id: number, updateDeliveryDto: UpdateDeliveryDto): Promise<Delivery | null>;
+    remove(id: number): Promise<void>;
 }
--- microservice-zone/src/location/dto/create-location.dto.ts
+++ microservice-zone/src/location/dto/create-location.dto.ts
@@ -1,1 +1,11 @@
-export class CreateLocationDto {}
+import {IsNumber, IsOptional} from 'class-validator';
+
+export class CreateLocationDto {
+  @IsNumber()
+  @IsOptional()
+  id?: number;
+  @IsNumber()
+  lat: number;
+  @IsNumber()
+  lng: number;
+}
