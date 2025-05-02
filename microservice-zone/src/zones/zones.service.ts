6b0ada3cb1714e6156abef5e5409361cbbfc8355
Julián <julianfermani45@gmail.com>
2025-05-02 00:35:41 -0300

    Método ❌ DELETE /zone/:id andando

--- microservice-zone/src/zones/zones.service.ts
+++ microservice-zone/src/zones/zones.service.ts
@@ -1,26 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateZoneDto } from './dto/create-zone.dto';
 import { UpdateZoneDto } from './dto/update-zone.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Zone } from './entities/zone.entity';
 
 @Injectable()
 export class ZonesService {
+  constructor(
+    @InjectRepository(Zone) private readonly zoneRepository: Repository<Zone>,
+  ) {}
+
   create(createZoneDto: CreateZoneDto) {
     return 'This action adds a new zone';
   }
 
-  findAll() {
-    return `This action returns all zones`;
+  async findAll() {
+    return await this.zoneRepository.find();
   }
 
   findOne(id: number) {
     return `This action returns a #${id} zone`;
   }
 
-  update(id: number, updateZoneDto: UpdateZoneDto) {
-    return `This action updates a #${id} zone`;
+  update(id: number) {
+    return `Butaso el que lee`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} zone`;
+  async remove(id: number): Promise<void> {
+    const zone = await this.zoneRepository.findOne({ where: { id } });
+
+    if (!zone) {
+      throw new NotFoundException(`Zone with id ${id} not found`);
+    }
+
+    await this.zoneRepository.remove(zone);
   }
 }
