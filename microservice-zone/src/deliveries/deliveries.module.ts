e1ce2b5ebf573d1719acb145346f025dbb4c97be
Julián <julianfermani45@gmail.com>
2025-05-01 21:52:54 -0300

    Resuelvo error de falta de modulos.

--- microservice-zone/src/deliveries/deliveries.module.ts
+++ microservice-zone/src/deliveries/deliveries.module.ts
@@ -3,8 +3,10 @@ import { DeliveriesService } from './deliveries.service';
 import { DeliveriesController } from './deliveries.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Delivery } from './entities/delivery.entity';
+import { Location } from 'src/location/entities/location.entity';
+import { DeliveryStatus } from 'src/delivery-status/entities/delivery-status.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([Delivery])],
+  imports: [TypeOrmModule.forFeature([Delivery, Location, DeliveryStatus])],
   controllers: [DeliveriesController],
   providers: [DeliveriesService],
 })
