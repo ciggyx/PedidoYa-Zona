f5197f69f4a064a53ef27b27e456b3b43d8443cb
Julián <julianfermani45@gmail.com>
2025-06-10 21:12:42 -0300

    Hotfix modulos

--- microservice-zone/src/deliveries/deliveries.module.ts
+++ microservice-zone/src/deliveries/deliveries.module.ts
@@ -6,8 +6,17 @@ import { Delivery } from './entities/delivery.entity';
 import { Location } from 'src/location/entities/location.entity';
 import { Zone } from 'src/zones/entities/zone.entity';
 import { DeliveryStatus } from 'src/delivery-status/entities/delivery-status.entity';
+import { DeliveryZone } from 'src/delivery-zone/entities/delivery-zone.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([Delivery, Location, DeliveryStatus, Zone])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Delivery,
+      Location,
+      DeliveryStatus,
+      Zone,
+      DeliveryZone,
+    ]),
+  ],
   controllers: [DeliveriesController],
   providers: [DeliveriesService],
 })
