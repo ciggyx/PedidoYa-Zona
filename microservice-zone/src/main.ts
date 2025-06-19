f1e98e4fe154f7ac256e51aadea86d353fce3d62
ciggy <alexandermassietti@gmail.com>
2025-06-19 14:29:26 -0300

    CORS agregado

--- microservice-zone/src/main.ts
+++ microservice-zone/src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //Agregamos un prefix o ruta base a todas las rutas de la API. Es opcional pero buena practica
   app.setGlobalPrefix('api/v1');
+  app.enableCors({
+    origin: 'http://localhost:4200',
+  });
 
   //Configramos las validaciones de los DTOs (Data transfer objects). Para que manden los datos correctos
   app.useGlobalPipes(
