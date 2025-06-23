02663c32d2c4c0bef806b03078a5e83da9cd6eeb
PIPICBA <lichimedina05@gmail.com>
2025-06-23 16:54:53 -0300

    Pequeño fix a delivery

--- microservice-zone/src/deliveries/dto/updateStatus.dto.ts
+++ microservice-zone/src/deliveries/dto/updateStatus.dto.ts
@@ -1,7 +1,7 @@
-import { IsString, IsIn } from 'class-validator';
+import { IsInt, Min } from 'class-validator';
 
 export class UpdateStatusDto {
-  @IsString()
-  @IsIn(['available', 'in_route'])
-  status: string;
+  @IsInt()
+  @Min(1)
+  statusId: number;
 }
